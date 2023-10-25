5df32733827bdf037e6f46760d06626923872e59
Max <scorpiomax90@gmail.com>
2023-10-25 21:25:59 +0300

    fixed settings json

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-	<header>
-		<ul>
+	<header class="bg-slate-600 shadow-border-custom">
+		<ul class="flex gap-4 py-5 container mx-auto">
 			<li v-for="item in links" :key="item">
-				<router-link class="" :to="item.url">Go to {{ item.link }}</router-link>
+				<router-link class="text-sky-600" :to="item.url">Go to {{ item.link }}</router-link>
 			</li>
 		</ul>
 	</header>
-  <div class="container">
+  <div class="container mx-auto">
 		<RouterView />
 	</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+		screens: {
+      sm: '520px',
+      md: '768px',
+      lg: '992px',
+      xl: '1310px',
+    },
     extend: {
 			boxShadow: {
-        'border': '0 0 0 1px rgba(71, 85, 105, 1)',
+        'border-custom': '0 0 5px 0 rgba(71, 85, 105, 1)',
       }
 		},
   },
