800bbfd3830bda12dd353c0932a6fa401102e959
Max <scorpiomax90@gmail.com>
2023-10-30 10:36:19 +0200

    added product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<article>
+			<div class="cursor-pointer">
+				<div class="h-80 bg-neutral-600">
+					<img loading="lazy" :src="props.cardProduct.thumbnail" :alt="props.cardProduct.title" class="h-full w-full object-cover">
+				</div>
+				<h2>Brand: {{ props.cardProduct.brand }}</h2>
+				<p>Description: {{ props.cardProduct.description }}</p>
+				<p>Price: ${{ props.cardProduct.price }}</p>
+			</div>
+		</article>
+</template>
+
+<script setup>
+
+	const props = defineProps({
+		cardProduct: {
+			type: Object,
+			required: true
+		}
+	})
+</script>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,24 +1,16 @@
 <template>
 	<div v-if="productsStore.products.length >= 1" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
-		<article
-			class=""
+		<ProductCard
 			v-for="product in productsStore.products"
 			:key="product.id"
+			:card-product="product"
 			@click="goToProductpage(product.id)"
-		>
-			<div class="cursor-pointer">
-				<div class="h-80 bg-neutral-600">
-					<img loading="lazy" :src="product.thumbnail" :alt="product.title" class="h-full w-full object-cover">
-				</div>
-				<h2>Brand: {{ product.brand }}</h2>
-				<p>Description: {{ product.description }}</p>
-				<p>Price: ${{ product.price }}</p>
-			</div>
-		</article>
+		/>
 	</div>
 </template>
 
 <script setup>
+	import ProductCard from '../components/ProductCard.vue'
 	import { useProductsStore } from '../store'
 	import { onMounted } from 'vue'
 	import { useRouter } from 'vue-router'
