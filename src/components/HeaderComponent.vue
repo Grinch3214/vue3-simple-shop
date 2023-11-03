3db26f1743703ff8b1921dc52509086973cc5ef2
Max <scorpiomax90@gmail.com>
2023-11-03 13:31:44 +0200

    added logo in project

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,13 @@
 <template>
 	<header class="shadow-border-custom">
 		<div class="py-5 container mx-auto px-4">
-			<ul class="flex justify-between">
+			<ul class="flex justify-between items-center">
 			<li>
-				<router-link to="/">Catalog</router-link>
+				<router-link to="/" class="w-[60px] h-[60px]">
+					<img src="@/assets/images/basket-logo.png" alt="basket-logo" width="60" height="60">
+				</router-link>
 			</li>
-			<li class="relative cursor-pointer" @click="openCart">
+			<li class="relative cursor-pointer mr-3" @click="openCart">
 				<span class="absolute -top-2 -right-3 bg-red-500/70 text-[12px] font-extrabold rounded-full w-5 h-5 flex items-center justify-center">{{ productsStore.cart.length }}</span>
 				<CartIcon width="30px" height="30px" />
 			</li>
