dd5f9236894bc9953d6a92a95e7967b66ea26de3
Max <scorpiomax90@gmail.com>
2023-11-04 01:02:02 +0200

    fixed place components

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,15 +1,19 @@
 <template>
-	<div v-if="productsStore.products.length >= 1" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
-		<ProductCard
-			v-for="product in productsStore.products"
-			:key="product.id"
-			:card-product="product"
-			@goToProduct="goToProductpage"
-		/>
-	</div>
+	<HeroComponent />
+	<section class="container mx-auto px-4">
+		<div v-if="productsStore.products.length >= 1" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+			<ProductCard
+				v-for="product in productsStore.products"
+				:key="product.id"
+				:card-product="product"
+				@goToProduct="goToProductpage"
+			/>
+		</div>
+	</section>
 </template>
 
 <script setup>
+	import HeroComponent from './HeroComponent.vue'
 	import ProductCard from '../components/ProductCard.vue'
 	import { useProductsStore } from '../store'
 	import { onMounted } from 'vue'
