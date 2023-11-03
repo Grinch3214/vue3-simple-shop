d87a8137c5a6faab53966c5399ada7a151ba8256
Max <scorpiomax90@gmail.com>
2023-11-03 13:43:10 +0200

    fixed style in header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,5 @@
 <template>
-	<header class="shadow-border-custom">
+	<header class="bg-white/60 fixed top-0 left-0 right-0 z-10 backdrop-blur-sm">
 		<div class="py-5 container mx-auto px-4">
 			<ul class="flex justify-between items-center">
 			<li>
@@ -9,7 +9,7 @@
 			</li>
 			<li class="relative cursor-pointer mr-3" @click="openCart">
 				<span class="absolute -top-2 -right-3 bg-red-500/70 text-[12px] font-extrabold rounded-full w-5 h-5 flex items-center justify-center">{{ productsStore.cart.length }}</span>
-				<CartIcon width="30px" height="30px" />
+				<CartIcon width="30px" height="30px" stroke="rgb(239 68 68 / 0.7)" />
 			</li>
 		</ul>
 		</div>
