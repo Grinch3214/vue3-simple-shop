31db353ee3a68d5ebc58b8ba68a594813b14f24a
Max <scorpiomax90@gmail.com>
2023-11-04 21:53:19 +0200

    added footer

--- src/views/FooterComponent.vue
+++ src/views/FooterComponent.vue
@@ -1,5 +1,7 @@
 <template>
-	<footer>
-		
+	<footer class="bg-gray-950 py-8 text-sm md:text-base">
+		<div class="container mx-auto px-4 text-white text-center">
+			<p>Copyright &copy; Eccomerce Shop 2023. All rights reserved.</p>
+		</div>
 	</footer>
 </template>
