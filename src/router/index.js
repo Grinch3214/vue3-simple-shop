bb5661974a9483fc39823e1080eed767e2782157
Max <scorpiomax90@gmail.com>
2023-11-21 12:12:25 +0200

    deleted CartComponent in routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Catalog from '../views/Catalog.vue'
 import ProductPage from '../components/ProductPage.vue'
-import CartComponent from '../components/CartComponent.vue'
 import NotFound from '../components/404.vue'
 
 const routes = [
@@ -14,11 +13,6 @@ const routes = [
 		path: '/product/:id',
 		name: 'ProductPage',
 		component: ProductPage
-	},
-	{
-		path: '/cart',
-		name: 'CartComponent',
-		component: CartComponent
 	},
 	{
 		path: '/:pathMatch(.*)*',
