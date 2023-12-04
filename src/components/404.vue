c7695d4703fb6e68e7353997f357f701e87b7cd8
Max <scorpiomax90@gmail.com>
2023-12-04 11:33:27 +0200

    added response for 404 page

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="pt-[120px] container mx-auto px-4 font-extrabold text-7xl text-red-500/70 h-full flex flex-col justify-center items-center">
+	<div class="pt-[120px] container mx-auto px-4 font-extrabold text-4xl md:text-7xl text-red-500/70 h-full flex flex-col justify-center items-center">
 		<p>404</p>
 		<p>Page not found</p>
 		<router-link to="/" class="transition px-4 py-2 bg-slate-950 text-white text-sm mt-5 hover:bg-black/80">Go to main page</router-link>
